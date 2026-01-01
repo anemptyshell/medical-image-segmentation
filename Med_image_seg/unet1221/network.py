da45b9c4cc7fed3240ebcd7009ca806eb9b269c0
anemptyshell <117813202+anemptyshell@users.noreply.github.com>
2026-01-01 17:48:57 +0800

    1.1-6

--- Med_image_seg/unet1221/network.py
+++ Med_image_seg/unet1221/network.py
@@ -338,8 +338,8 @@ class EnhancedFusionWithSqueeze(nn.Module):
         # 4. 计算补集并加权
         complement = 1.0 - preds                # [bs, 1, h, w]
         complement_weighted = complement * w    # [bs, 1, h, w]
-        # complement_weighted += preds    ## 此处改动记为add   效果很差 几乎为0
-        complement_weighted += complement   ## 此处改动记为add1
+        # complement_weighted += preds      ## 此处改动记为add   效果很差 几乎为0
+        complement_weighted += complement   ## 此处改动记为add1  指标提升，但w可视化几乎没有
         return w, complement_weighted
 
 
